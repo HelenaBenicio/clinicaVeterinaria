22f93fe438bedb5eae874aa085065a03cf5019a6
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2022-10-12 14:26:58 -0300

    ajustes

--- animal/models.py
+++ animal/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Animal(models.Model):
-    nome = models.CharField(max_length=256),
+    nome_pet = models.CharField(max_length=256),
     data_de_nascimento = models.DateField(),
     idade = models.IntegerField(),
     especie = models.CharField(max_length=20),
